7ee78db1d63d27e0542aeac303231ff9e54a23b6
william <william@ubuntu>
2015-01-11 04:41:37 -0800

    final

--- index.php
+++ index.php
@@ -11,9 +11,10 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include('Student.php');
-        $students = array();
+        include('Student.php'); // import Student.php class
+        $students = array();    // create student array to store each student 
 
+        // add 1st student
         $first = new Student();
         $first->surname = "Doe";
         $first->first_name = "John";
@@ -22,8 +23,9 @@ and open the template in the editor.
         $first->add_grade(65);
         $first->add_grade(75);
         $first->add_grade(55);
-        $students['j123'] = $first;
+        $students['j123'] = $first; // give the student an index
 
+        // add 2nd student
         $second = new Student();
         $second->surname = "Einstein";
         $second->first_name = "Albert";
@@ -34,8 +36,18 @@ and open the template in the editor.
         $second->add_grade(80);
         $second->add_grade(50);
         $students['a456'] = $second;    
+        
+        // add 3rd student
+        $third = new Student();
+        $third->surname = "Parry";
+        $third->first_name = "Jim";
+        $third->add_email('work', 'jim_parry@bcit.ca');
+        $third->add_grade(85);
+        $students['p789'] = $third;
+        
+        ksort($students); // the sorting function, in alphabetic order
 
-        foreach ($students as $student) {
+        foreach ($students as $student) { // print each student in a format
             echo $student->toString();
         }
         ?>
