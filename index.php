34566e7a3a8a4125e9aba097a4b4ffd3b10119eb
william <william@ubuntu>
2015-01-11 04:12:14 -0800

    Added first and second students

--- index.php
+++ index.php
@@ -11,7 +11,33 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "This is test for setup"
+        include('student.php');
+
+        $students = array();
+
+        foreach ($students as $student)
+            echo $student->toString();
+
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home', 'john@doe.com');
+        $first->add_email('work', 'jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home', 'albert@braniacs.com');
+        $second->add_email('work1', 'a_einstein@bcit.ca');
+        $second->add_email('work2', 'albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
         ?>
     </body>
 </html>
