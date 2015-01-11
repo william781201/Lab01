9ac01d5cdb8d23b598c09ec2b7818b2e96500114
william <william@ubuntu>
2015-01-11 04:47:06 -0800

    final

--- Student.php
+++ Student.php
@@ -14,32 +14,39 @@
 class Student {
     
     function _construct() {
-        $this->surname = '';
-        $this->first_name = '';
-        $this->emails = array();
-        $this->grades = array();
+        $this->surname = '';        // last name
+        $this->first_name = '';     // first name
+        $this->emails = array();    // email
+        $this->grades = array();    // grades
     }
     
     function add_email($which, $address) {
-        $this->emails[$which] = $address;
+        $this->emails[$which] = $address; // address stores in the emails array
     }
     
     function add_grade($grade) {
-        $this->grades[] = $grade;
+        $this->grades[] = $grade; // grade stores in the grades array
     }
     
     function average() {
         $total = 0;
-        foreach ($this->grades as $value)
+        foreach ($this->grades as $value) {
             $total += $value;
-        return $total / count($this->grade);
+        }
+        
+        // the sum devided by the # of grade
+        return $total / count($this->grades); 
     }
     
+    // formats the student info
     function toString() {
         $result = $this->first_name . '' . $this->surname;
-        $result .= ' ('. $this->average() . ")\n";
-        foreach ($this->emails as $which=>$what)
+        $result .= ' ('. $this->average() . ") ";
+        
+        // which is the index in emails
+        foreach ($this->emails as $which => $what) {
             $result .= $which . ': ' . $what . "\n";
+        }
         $result .= "\n";
         return '<pre>' . $result . '</pre>';            
     }
